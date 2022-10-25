22c6ecc891fcabccbaaa3b6cccbf3562fff872c9
kitdea <francismarrosales@gmail.com>
2022-10-25 17:15:25 +0800

    script revisions

--- js/app.js
+++ js/app.js
@@ -10,88 +10,49 @@ const computerChoiceDisplay = document.getElementById('computer-choice');
 const userChoiceDisplay = document.getElementById('user-choice');
 const resultDisplay = document.getElementById('result');
 const possibleChoices = document.querySelectorAll('button');
-let userChoice;
-let computerChoice;
+let playerSelection ;
+let computerSelection;
 let result;
 
 possibleChoices.forEach(possibleChoice => possibleChoice.addEventListener('click', (e) => {
-  userChoice = e.target.id
-  userChoiceDisplay.innerHTML = userChoice;
-  generateComputerChoice();
-  getResult();
+  playerSelection = e.target.id
+  userChoiceDisplay.innerHTML = playerSelection;
+  getComputerChoice();
+  game();
 }));
 
-function generateComputerChoice() {
+function getComputerChoice() {
   const randomNumber = Math.floor(Math.random() * 3) + 1;
 
   if(randomNumber === 1) {
-    computerChoice = 'rock';
+    computerSelection = 'rock';
   }
 
   else if(randomNumber === 2) {
-    computerChoice = 'scissor';
+    computerSelection = 'scissor';
   }
 
   else if(randomNumber === 3) {
-    computerChoice = 'paper';
+    computerSelection = 'paper';
   }
-  computerChoiceDisplay.innerHTML = computerChoice;
+  computerChoiceDisplay.innerHTML = computerSelection;
 }
 
-function getResult() {
-  if(computerChoice === userChoice) {
+function game() {
+  if(computerSelection === playerSelection) {
     result = 'Its a draw!';
-  } else if (computerChoice === 'rock' && userChoice === 'paper') {
+  } else if (computerSelection === 'rock' && playerSelection === 'paper') {
     result = 'You win!';
-  } else if (computerChoice === 'rock' && userChoice === 'scissor') {
+  } else if (computerSelection === 'rock' && playerSelection === 'scissor') {
     result = 'You lose!';
-  } else if (computerChoice === 'paper' && userChoice === 'scissor') {
+  } else if (computerSelection === 'paper' && playerSelection === 'scissor') {
     result = 'You win!';
-  } else if (computerChoice === 'paper' && userChoice === 'rock') {
+  } else if (computerSelection === 'paper' && playerSelection === 'rock') {
     result = 'You lose';
-  } else if (computerChoice === 'scissor' && userChoice === 'rock') {
+  } else if (computerSelection === 'scissor' && playerSelection === 'rock') {
     result = 'You win!';
-  } else if (computerChoice === 'scissor' && userChoice === 'paper') {
+  } else if (computerSelection === 'scissor' && playerSelection === 'paper') {
     result = 'You lose!';
   }
-  console.log(result);
   resultDisplay.innerHTML = result;
 };
-
-// Computer random choice
-const choiceSelection = ['rock', 'paper', 'scissor'];
-
-const getComputerChoice = (choiceSelection) => {
-  let numberOfChoices = choiceSelection.length;
-  let randomAnswer = Math.floor(Math.random() * numberOfChoices);
-  let itemChoice = choiceSelection[randomAnswer];
-  return itemChoice + ' ang pamato ni Kumander Bawang';
-};
-let computerResponse = getComputerChoice(choiceSelection);
-// console.log(computerResponse);
-
-// Play the game
-const select = ['rock', 'paper', 'scissor'];
-
-const playerChoice = (select) => {
-  let choices = select.length;
-  let randomChoice = Math.floor(Math.random() * choices);
-  let selectChoice = select[randomChoice];
-  return selectChoice + ' ang pamato ni Heneral Sibuyas';
-};
-let playerResponse = playerChoice(select);
-
-const playRound = (playerSelection, computerSelection) => {
-  (playerSelection === computerSelection) ?
-    console.log('Its a tie') : console.log('Not')
-  
-};
-
-const computerSelection = computerResponse;
-const playerSelection = playerResponse;
-const palakat = playRound(playerSelection, computerSelection);
-console.log(playerSelection);
-console.log(computerSelection);
-console.log(palakat);
-
-
